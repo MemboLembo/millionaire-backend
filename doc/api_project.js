b9172a38db5a7dbfbbcc61b5aa57fcd5b5f9a2d5
Vasiliy Gevko <v.gevko@bmlabs.org>
2021-03-29 22:55:27 +0300

    Created doc

---
+++ doc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "millionaire-backend",
+  "version": "1.0.0",
+  "description": "Millionaire test task api",
+  "title": "Millionaire Backend",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-03-29T19:55:04.180Z",
+    "url": "https://apidocjs.com",
+    "version": "0.27.1"
+  }
+});
